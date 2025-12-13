edf0f720dc7bb2164265b55138c7c36320fe867b
Esc <vasercom7@gmail.com>
2025-12-13 13:36:48 +0300

    Implemented new method AddEmployee

--- Program.cs
+++ Program.cs
@@ -1,8 +1,70 @@
-﻿using Spectre.Console;
+﻿using System.Globalization;
+using Spectre.Console;
 namespace AssistLabTask1;
+
+public class Employee
+{
+    public int ID { get; set; }
+    public required string FullName { get; set; }
+    public string? Position { get; set; }
+    public DateTime HireDate { get; set; }
+    public bool IsRemote { get; set; }
+}
+
 
 public class Program
 {
+    private static readonly List<Employee> employees = [];
+    private static readonly Table employeesTable = new();
+    public static void AddEmployee()
+    {
+        while (true)
+        {
+            Console.Write("Insert employee's full name: ");
+            string? nameInput = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(nameInput))
+            {
+                AnsiConsole.MarkupLine("[red]Full name can't be null![/]");
+                continue;
+            }
+            var employee = new Employee() { FullName = nameInput };
+            Console.Write("Insert employee's hire date: ");
+            string? positionInput = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(nameInput))
+            {
+                AnsiConsole.MarkupLine("[red]Position was specified, setting it to \"-\"[/]");
+                
+            }
+            string? dateInput = Console.ReadLine();
+            if (DateTime.TryParseExact(dateInput, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
+            {
+                employee.HireDate = result;
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red]Hire date is invalid![/]");
+                continue;
+            }
+            Console.Write("Is remote employee(y/n, yes/no, 1/0): ");
+            string? isRemote = Console.ReadLine()?.Trim().ToLower();
+            switch (isRemote)
+            {
+                case "y" or "yes" or "1":
+                    employee.IsRemote = true;
+                    break;
+                case "n" or "no" or "0":
+                    employee.IsRemote = false;
+                    break;
+                default:
+                    AnsiConsole.MarkupLine("[red]Invalid input![/]");
+                    continue;
+            }
+            employee.ID = employees.Count + 1;
+            employees.Add(employee);
+            AnsiConsole.MarkupLine($"[green]New employee {employee.FullName} is added successfully![/]");
+            break;
+        }
+    }
     static string GetInput()
     {
         while (true)
@@ -26,7 +88,7 @@ public class Program
             switch (input)
             {
                 case "1":
-                    // Add a new employee logic to be added... 
+                    AddEmployee();
                     break;
                 case "2":
                     // List all employees logic to be added...
