9a77be7b0897e1a83ebc7d6c13b64e753f9d24ab
Esc <vasercom7@gmail.com>
2025-12-16 00:44:30 +0300

    Refactored the entire code structure to match onion architecture

---
+++ Application/ApplicationClass.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeListManager.Domain;
+namespace EmployeeListManager.Application;
+
+public class ApplicationClass
+{
+    private readonly IEmployeeRepository _repository;
+    public ApplicationClass(IEmployeeRepository repository) => _repository = repository;
+    public void AddEmployee(Employee employee)
+    {
+        _repository.Add(employee);
+    }
+    public IReadOnlyList<Employee> GetAllEmployees()
+    {
+        return _repository.GetAllEmployees();
+    }
+    public IReadOnlyList<Employee> GetRemoteEmployees()
+    {
+        return [.. _repository.GetAllEmployees().Where(e => e.IsRemote == true)];
+    }
+    public Employee? FindById(int id)
+    {
+        return _repository.FindById(id);
+    }
+}
---
+++ Infrastructure/InfrastructureClass.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeListManager.Domain;
+namespace EmployeeListManager.Infrastructure;
+
+public class InfrastructureClass : IEmployeeRepository
+{
+    private readonly List<Employee> _employees = [];
+    private int _nextId = 1;
+    public void Add(Employee employee)
+    {
+        employee.Id = _nextId++;
+        _employees.Add(employee);
+    }
+    public IReadOnlyList<Employee> GetAllEmployees()
+    {
+        return _employees;
+    }
+    public Employee? FindById(int id)
+    {
+        return _employees.FirstOrDefault(e => e.Id == id);
+    }
+}
